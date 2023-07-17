7c3ca27ab88210c99ef5e898d107b3c8d840e650
TalusL <xiaoxiaochenjian@gmail.com>
2023-07-17 18:34:49 +0800

    add agent mgr

---
+++ src/AgentMgr.cpp
@@ -0,0 +1,5 @@
+//
+// Created by liangzhuohua on 2023/7/17.
+//
+
+#include "AgentMgr.h"
---
+++ src/AgentMgr.h
@@ -0,0 +1,14 @@
+//
+// Created by liangzhuohua on 2023/7/17.
+//
+
+#ifndef TALUSPBX_AGENTMGR_H
+#define TALUSPBX_AGENTMGR_H
+
+
+class AgentMgr {
+
+};
+
+
+#endif //TALUSPBX_AGENTMGR_H
